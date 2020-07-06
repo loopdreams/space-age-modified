84a284be3339b87e3e3735b9821c9c053da2b786
Gary Johnson <lambdatronic@gmail.com>
2020-07-05 20:23:03 -0400

    URL-decoded query params and moved response functions to space-age.responses.

--- src/space_age/handler.clj
+++ src/space_age/handler.clj
@@ -1,67 +1,48 @@
 (ns space-age.handler
+  (:import java.net.URLDecoder
+           java.io.UnsupportedEncodingException
+           java.nio.charset.StandardCharsets)
   (:require [clojure.string :as str]
-            [space-age.logging :refer [log]]))
+            [space-age.logging :refer [log]]
+            [space-age.responses :refer [mime-type
+                                         success-response
+                                         permanent-failure-response]]))
 
-(def gemini-uri-regex #"^([a-z]+)://([^/]+)([^\?]*)(\?.+)?$")
+(def utf-8 (.name StandardCharsets/UTF_8))
 
-;; FIXME: %-decode k and v
+(defn url-decode [s]
+  (URLDecoder/decode s utf-8))
+
 (defn parse-query [query]
   (if query
     (let [kv-pairs (-> query
                        (subs 1)
                        (str/split #"&"))]
-      (reduce (fn [acc kv-pair]
-                (let [[k v] (str/split kv-pair #"=")]
-                  (assoc acc k v)))
-              {}
-              kv-pairs))
+      (try
+        (reduce (fn [acc kv-pair]
+                  (let [[k v] (str/split kv-pair #"=")]
+                    (assoc acc (url-decode k) (url-decode v))))
+                {}
+                kv-pairs)
+        (catch UnsupportedEncodingException e {})))
     {}))
+
+(def gemini-uri-regex #"^([a-z]+)://([^/]+)([^\?]*)(\?.+)?$")
 
 (defn parse-uri [uri]
   (when-let [[uri scheme host path query] (->> uri
                                                (str/trim)
                                                (str/lower-case)
                                                (re-find gemini-uri-regex))]
-    {:uri    uri
-     :scheme scheme
+    {:scheme scheme
      :host   host
      :path   path
      :params (parse-query query)}))
 
-(defn query-response [prompt]
-  (str "10 " prompt "\r\n"))
-
-;; FIXME: Handle binary data in body
-(defn success-response [type body]
-  (str "20 " type "\r\n" body))
-
-(defn redirect-response [uri]
-  (str "30 " uri "\r\n"))
-
-(defn temporary-failure-response [msg]
-  (str "40 " msg "\r\n"))
-
-(defn permanent-failure-response [msg]
-  (str "50 " msg "\r\n"))
-
 ;; FIXME: stub
-(defn client-certificate-required-response []
-  (str "60\r\n"))
-
-;; FIXME: Verify that these are correct
-(def mime-type {"gmi"    "text/gemini; charset=utf-8"
-                "gemini" "text/gemini; charset=utf-8"
-                "txt"    "text/plain; charset=utf-8"
-                "html"   "text/html; charset=utf-8"
-                "png"    "image/png"
-                "gif"    "image/gif"
-                "jpg"    "image/jpeg"
-                "jpeg"   "image/jpeg"})
-
-;; FIXME: stub
-;; Example URI: gemini://myhost.org/foo/bar?baz=buzz&boz=bazizzle
+;; Example URI: gemini://myhost.org/foo/bar?baz=buzz&boz=bazizzle\r\n
 (defn gemini-handler [uri]
   (log uri)
-  (if-let [{:keys [uri scheme host path params] :as request} (parse-uri uri)]
+  (if-let [{:keys [scheme host path params] :as request} (parse-uri uri)]
     (success-response (mime-type "txt") (str request))
     (permanent-failure-response "Malformed URI")))
---
+++ src/space_age/responses.clj
@@ -0,0 +1,31 @@
+(ns space-age.responses)
+
+(defn query-response [prompt]
+  (str "10 " prompt "\r\n"))
+
+;; FIXME: Handle binary data in body
+(defn success-response [type body]
+  (str "20 " type "\r\n" body))
+
+(defn redirect-response [uri]
+  (str "30 " uri "\r\n"))
+
+(defn temporary-failure-response [msg]
+  (str "40 " msg "\r\n"))
+
+(defn permanent-failure-response [msg]
+  (str "50 " msg "\r\n"))
+
+;; FIXME: stub
+(defn client-certificate-required-response []
+  (str "60\r\n"))
+
+;; FIXME: Verify that these are correct
+(def mime-type {"gmi"    "text/gemini; charset=utf-8"
+                "gemini" "text/gemini; charset=utf-8"
+                "txt"    "text/plain; charset=utf-8"
+                "html"   "text/html; charset=utf-8"
+                "png"    "image/png"
+                "gif"    "image/gif"
+                "jpg"    "image/jpeg"
+                "jpeg"   "image/jpeg"})

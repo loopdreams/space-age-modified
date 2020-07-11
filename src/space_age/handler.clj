77927285cc10c1094f86f5b0203e3bd7832e43ed
Gary Johnson <lambdatronic@gmail.com>
2020-07-11 19:54:14 -0400

    Rewrote space-age.handler/parse-uri to use java.net.URI.

--- src/space_age/handler.clj
+++ src/space_age/handler.clj
@@ -1,41 +1,24 @@
 (ns space-age.handler
-  (:import java.net.URLDecoder
-           java.nio.charset.StandardCharsets)
+  (:import java.net.URI)
   (:require [clojure.string :as str]
             [space-age.logging :refer [log]]
             [space-age.responses :refer [permanent-failure-response
                                          process-request]]))
 
-(def utf-8 (.name StandardCharsets/UTF_8))
-
-(defn url-decode [s]
-  (URLDecoder/decode s utf-8))
-
+;; FIXME: Do we expect kv pairs using Gemini?
 (defn parse-query [query]
   (if (str/blank? query)
     {}
-    (let [kv-pairs (-> query
-                       (subs 1)
-                       (str/split #"&"))]
-      (reduce (fn [acc kv-pair]
-                (let [[k v] (str/split kv-pair #"=")]
-                  (assoc acc (url-decode k) (url-decode v))))
-              {}
-              kv-pairs))))
-
-(def gemini-uri-regex #"^([A-Za-z]+)://([^/]+)/?([^\?]*)(\?.+)?$")
+    (apply hash-map (str/split query #"&|="))))
 
 (defn parse-uri [uri]
   (try
-    (when-let [[_ scheme host+port route query] (->> uri
-                                                     (str/trim)
-                                                     (re-find gemini-uri-regex))]
-      (let [[host port] (str/split host+port #":")]
-        {:scheme  (str/lower-case scheme)
-         :host    (str/lower-case host)
-         :port    (if port (Integer/parseInt port) 1965)
-         :route   route
-         :params  (parse-query query)}))
+    (let [uri-obj (URI/create (str/trim uri))]
+      {:scheme (str/lower-case (.getScheme uri-obj))
+       :host   (str/lower-case (.getHost uri-obj))
+       :port   (if (pos? (.getPort uri-obj)) (.getPort uri-obj) 1965)
+       :route  (.getPath uri-obj)
+       :params (parse-query (.getQuery uri-obj))})
     (catch Exception e nil)))
 
 ;; Example URI: gemini://myhost.org/foo/bar?baz=buzz&boz=bazizzle\r\n

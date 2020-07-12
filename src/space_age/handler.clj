c5ae82005d3d03f56e03e8de4c935bdf6e1b5da1
Gary Johnson <lambdatronic@gmail.com>
2020-07-12 02:58:01 -0400

    Changed script runner API to run a "main" fn with request map and expect a response map to be returned.

--- src/space_age/handler.clj
+++ src/space_age/handler.clj
@@ -8,6 +8,30 @@
                                          temporary-failure-response
                                          permanent-failure-response]]))
 
+(defn valid-response? [response]
+  (and (map? response)
+       (contains? #{10 20 30 40 50 60} (:status response))
+       (string? (:meta response))
+       (or (nil? (:body response))
+           (string? (:body response))
+           (instance? File (:body response)))))
+
+(defn run-clj-script [^File file request]
+  (let [script-ns-name (gensym "script")]
+    (try
+      (binding [*ns* (create-ns script-ns-name)]
+        (refer-clojure)
+        (with-out-str (load-file (.getPath file))) ; mute script's stdout
+        (if-let [main-fn (resolve 'main)]
+          (let [response (main-fn request)]
+            (if (valid-response? response)
+              response
+              (permanent-failure-response "Script error: Invalid response.")))
+          (permanent-failure-response "Script error: No main function.")))
+      (catch Exception e
+        (permanent-failure-response (str "Script error: " (.getMessage e))))
+      (finally (remove-ns script-ns-name)))))
+
 (defn make-directory-listing [path ^File directory]
   (let [dir-name  (last (remove str/blank? (str/split path #"/")))
         dir-label (str/replace (str "/" path "/") #"/+" "/")]
@@ -16,18 +40,6 @@
          (sort)
          (str/join "\n")
          (str "Directory Listing: " dir-label "\n\n"))))
-
-(defn run-clj-script [^File file request]
-  (let [script-ns-name (gensym "script")]
-    (try
-      (binding [*ns* (create-ns script-ns-name)]
-        (refer-clojure)
-        (intern *ns* '*gemini-request* request)
-        (let [script-output (with-out-str (load-file (.getPath file)))]
-          (success-response (get-mime-type "script.gmi") script-output)))
-      (catch Exception e
-        (permanent-failure-response (str "Script error: " (.getMessage e))))
-      (finally (remove-ns script-ns-name)))))
 
 (defn path->file [document-root path]
   (let [path (if (str/starts-with? path "/") (subs path 1) path)]
@@ -44,9 +56,10 @@
 ;; directory or the document-root directory. If a directory is
 ;; requested, we serve up its index.gmi or index.gemini if available.
 ;; Otherwise, we make a directory listing of links. If an executable
-;; *.clj file is requested, it is run in its own temporary namespace,
-;; and anything printed to standard output is returned as the body of
-;; a text/gemini response.
+;; *.clj file is requested, it is loaded into its own temporary
+;; namespace and its "main" function (if any) is run. If the return
+;; value is a valid Gemini response, it is returned to the client.
+;; Otherwise, an error response is returned.
 (defn process-request [document-root {:keys [path] :as request}]
   (try
     (let [file (path->file document-root path)]

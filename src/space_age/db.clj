5d11df56936a93f8656f5809ef9b5508ef1cfadf
Eoin Carney <eoincarney0@gmail.com>
2024-05-09 12:03:23 +0100

    refactor dbs into one

--- src/space_age/db.clj
+++ src/space_age/db.clj
@@ -4,11 +4,7 @@
             [clojure.string :as str]
             [java-time.api :as jt]))
 
-;; TODO refactor into single db connection
-(def db_users (jdbc/get-datasource {:dbtype "sqlite" :dbname "data/users.db"}))
-(def db_chat (jdbc/get-datasource {:dbtype "sqlite" :dbname "data/chat.db"}))
-(def db_wordle (jdbc/get-datasource {:dbtype "sqlite" :dbname "data/wordle.db"}))
-(def db_chess (jdbc/get-datasource {:dbtype "sqlite" :dbname "data/chess.db"}))
+(def db_games (jdbc/get-datasource {:dbtype "sqlite" :dbname "data/games.db"}))
 
 ;; word list from https://github.com/charlesreid1/five-letter-words/blob/master/sgb-words.txt
 (defonce wordle-words
@@ -16,51 +12,52 @@
       (str/split #"\n")
       shuffle))
 
+(defn drop-table! [table]
+  (jdbc/execute! db_games [(str "DROP TABLE IF EXISTS " table)]))
+
+(defn create-table! [spec]
+  (jdbc/execute! db_games spec))
+
 (defn init-dbs! []
-  (jdbc/execute! db_users ["drop table if exists users"])
-  (jdbc/execute! db_chat ["drop table if exists messages"])
-  (jdbc/execute! db_wordle ["drop table if exists games"])
-  (jdbc/execute! db_wordle ["drop table if exists words"])
-  (jdbc/execute! db_chess ["drop table if exists games"])
-  (jdbc/execute! db_users
-                 ["create table users (userid integer not null primary key,
+  (doall
+   (map drop-table!
+        ["users" "messages" "wordlegames" "wordlewords" "chessgames"]))
+  (map create-table!
+       [["create table users (userid integer not null primary key,
                                            username varchar(255),
                                            cert varchar(255),
-                                           joined datetime default current_timestamp)"])
-  (jdbc/execute! db_chat
-                 ["create table messages (msgid integer not null primary key,
+                                           joined datetime default current_timestamp)"]
+        ["create table messages (msgid integer not null primary key,
                                               username varchar(255),
                                               message varchar(255),
-                                              time varchar(255))"])
-  (jdbc/execute! db_wordle
-                 ["create table games (gameid integer not null primary key,
-                                           gamedate datetime default current_timestamp,
-                                           uid varchar(255),
-                                           guesses varchar(255),
-                                           score integer,
-                                           win integer default 0)"])
-  (jdbc/execute! db_wordle
-                 ["create table words (wordid integer not null primary key,
-                                           word varchar(10),
-                                           day datetime)"])
-  (jdbc/execute! db_chess
-                 ["create table games (rowid integer not null primary key,
-                                           gameid integer,
-                                           startdate datetime default current_timestamp,
-                                           startedby varchar(255),
-                                           enddate datetime,
-                                           playerturn varchar(10) default 'white',
-                                           whiteID varchar(255),
-                                           blackID varchar(255),
-                                           boardstate varchar(255),
-                                           complete integer default 0,
-                                           winner varchar(255))"]))
+                                              time varchar(255))"]
+        ["create table wordlegames (gameid integer not null primary key,
+                                                 gamedate datetime default current_timestamp,
+                                                 uid varchar(255),
+                                                 guesses varchar(255),
+                                                 score integer,
+                                                 win integer default 0)"]
+        ["create table wordlewords (wordid integer not null primary key,
+                                                 word varchar(10),
+                                                 day datetime)"]
+        ["create table chessgames (rowid integer not null primary key,
+                                                gameid integer,
+                                                startdate datetime default current_timestamp,
+                                                startedby varchar(255),
+                                                enddate datetime,
+                                                playerturn varchar(10) default 'white',
+                                                whiteID varchar(255),
+                                                blackID varchar(255),
+                                                boardstate varchar(255),
+                                                complete integer default 0,
+                                                winner varchar(255))"]]))
+        
 
 (defn init-words! [word-list]
   (let [start-date (atom (jt/local-date))]
     (for [w word-list]
       (do
-        (sql/insert! db_wordle :words {:word w :day (str @start-date)})
+        (sql/insert! db_games :wordleWords {:word w :day (str @start-date)})
         (swap! start-date #(jt/plus % (jt/days 1)))
         nil))))
 
@@ -78,120 +75,127 @@
 
 ;; USERS
 (defn register-user! [cert name]
-  (sql/insert! db_users :users {:cert cert :username name}))
+  (sql/insert! db_games :users {:cert cert :username name}))
 
 (defn update-name! [cert name]
-  (sql/update! db_users :users {:username name} {:cert cert}))
+  (sql/update! db_games :users {:username name} {:cert cert}))
 
 (defn get-username [req]
   (let [cert (client-id req)]
     (->
-     (sql/query db_users ["select username from users where cert = ?" cert])
+     (sql/query db_games ["select username from users where cert = ?" cert])
      first
      :users/username)))
 
 (defn get-username-by-id [id]
-  (-> (sql/query db_users ["SELECT username FROM users WHERE cert = ?" id])
+  (-> (sql/query db_games ["SELECT username FROM users WHERE cert = ?" id])
       first
       :users/username))
 
 (defn all-users []
-  (->> (sql/query db_users ["select username from users"])
+  (->> (sql/query db_games ["select username from users"])
        (map :users/username)
        (into #{})))
 
 ;; CHAT
 (defn chat-insert-message! [message]
-  (sql/insert! db_chat :messages message))
+  (sql/insert! db_games :messages message))
 
 (defn chat-get-messages []
-  (sql/query db_chat ["select * from messages"]))
+  (sql/query db_games ["select * from messages"]))
 
 
 ;; WORDLE
 
 (defonce valid-words
-  (->> (sql/query db_wordle ["SELECT word FROM words"])
-      (map :words/word)
+  (->> (sql/query db_games ["SELECT word FROM wordlewords"])
+      (map :wordlewords/word)
       (into #{})))
 
 (defn get-todays-word []
-  (-> (sql/query db_wordle ["SELECT word FROM words WHERE day = strftime('%Y-%m-%d', date('now'))"])
+  (-> (sql/query db_games ["SELECT word FROM wordlewords WHERE day = strftime('%Y-%m-%d', date('now'))"])
       first
-      :words/word))
+      :wordlewords/word))
 
 (defn get-today-q [col req]
   (let [uid (client-id req)]
     [(str "SELECT " col
-          " FROM games WHERE uid = ? AND strftime('%Y-%m-%d', gamedate) = strftime('%Y-%m-%d', date('now'))")
+          " FROM wordlegames WHERE uid = ? AND strftime('%Y-%m-%d', gamedate) = strftime('%Y-%m-%d', date('now'))")
      uid]))
 
 (defn get-guesses [req]
-  (-> (sql/query db_wordle (get-today-q "guesses" req))
+  (-> (sql/query db_games (get-today-q "guesses" req))
       first
       :games/guesses))
 
 (defn get-game-id [req]
-  (-> (sql/query db_wordle (get-today-q "gameid" req))
+  (-> (sql/query db_games (get-today-q "gameid" req))
       first
       :games/gameid))
 
 (defn get-score [req]
-  (-> (sql/query db_wordle (get-today-q "score" req))
+  (-> (sql/query db_games (get-today-q "score" req))
       first
       :games/score))
 
 (defn win-condition [req]
-  (-> (sql/query db_wordle (get-today-q "win" req))
+  (-> (sql/query db_games (get-today-q "win" req))
       first
       :games/win))
 
 (defn update-win-condition! [req]
-  (sql/update! db_wordle :games {:win 1} {:gameid (get-game-id req)}))
+  (sql/update! db_games :wordlegames {:win 1} {:gameid (get-game-id req)}))
 
 (defn insert-guess! [req guess-str]
   (if (get-guesses req)
     (let [game-id (get-game-id req)]
-      (sql/update! db_wordle :games {:guesses guess-str} {:gameid game-id})
-      (jdbc/execute! db_wordle ["UPDATE games SET score = score + 1 WHERE gameid = ?" game-id]))
-    (sql/insert! db_wordle :games {:uid (client-id req) :guesses guess-str :score 1})))
+      (sql/update! db_games :wordlegames {:guesses guess-str} {:gameid game-id})
+      (jdbc/execute! db_games ["UPDATE wordlegames SET score = score + 1 WHERE gameid = ?" game-id]))
+    (sql/insert! db_games :wordlegames {:uid (client-id req) :guesses guess-str :score 1})))
 
 (defn user-stats [req]
-  (sql/query db_wordle ["SELECT * FROM games WHERE uid = ?" (client-id req)]))
+  (sql/query db_games ["SELECT * FROM wordlegames WHERE uid = ?" (client-id req)]))
 
 
 ;; CHESS
 
 (defn init-game [req board colour gameid]
   (let [uid (client-id req)]
-    (sql/insert! db_chess :games {colour uid
-                                  :boardstate board
-                                  :gameid gameid
-                                  :startedby uid})))
+    (sql/insert! db_games :chessgames {colour uid
+                                       :boardstate board
+                                       :gameid gameid
+                                       :startedby uid})))
 
 (defn player-join [req gameid colour]
-  (sql/update! db_chess :games {colour (client-id req)} {:gameid gameid}))
+  (sql/update! db_games :chessgames {colour (client-id req)} {:gameid gameid}))
 
 
 
 
 (defn get-gameinfo [gameid]
-  (sql/query db_chess ["SELECT * FROM games WHERE gameid = ?" gameid]))
+  (sql/query db_games ["SELECT * FROM chessgames WHERE gameid = ?" gameid]))
 
-(defn update-board! [gameid board]
-  (let [next-player (if (= (:games/playerturn (first (get-gameinfo gameid))) "white")
+(defn update-board! [gameid board win?]
+  (let [next-player (if (= (:chessgames/playerturn (first (get-gameinfo gameid))) "white")
                       "black"
                       "white")]
-    (sql/update! db_chess :games {:boardstate board :playerturn next-player} {:gameid gameid})))
+    (println win?)
+    (do
+      (sql/update! db_games :chessgames {:boardstate board :playerturn next-player} {:gameid gameid})
+      (when win?
+        (println "here")
+        (sql/update! db_games :chessgames
+                     {:complete 1 :winner (if (= next-player "white") "black" "white")}
+                     {:gameid gameid})))))
 
 (defn get-active-games [req]
   (let [uid              (client-id req)
-        all-active-games (sql/query db_chess ["SELECT * FROM games WHERE complete = 0"])
-        player-games     (filter #(or (= (:games/whiteID %) uid)
-                                      (= (:games/blackID %) uid))
+        all-active-games (sql/query db_games ["SELECT * FROM chessgames WHERE complete = 0"])
+        player-games     (filter #(or (= (:chessgames/whiteID %) uid)
+                                      (= (:chessgames/blackID %) uid))
                                  all-active-games)
-        open-games       (->> (filter #(or (not (:games/whiteID %))
-                                           (not (:games/blackID %)))
+        open-games       (->> (filter #(or (not (:chessgames/whiteID %))
+                                           (not (:chessgames/blackID %)))
                                       all-active-games)
                               (remove #(some #{%} player-games)))
         running-games    (->> all-active-games
@@ -204,18 +208,11 @@
 (defn get-player-type [req gameid]
   (let [uid (client-id req)
         game (first (get-gameinfo gameid))
-        whiteID (:games/whiteID game)
-        blackID (:games/blackID game)]
+        whiteID (:chessgames/whiteID game)
+        blackID (:chessgames/blackID game)]
     (cond
       (= whiteID uid) :white
       (= blackID uid) :black
       :else "Something went wrong")))
 
 
-(comment
-  (insert-guess! "Johnny" "yoyo")
-  (get-game-id "Mary")
-  (get-guesses "Mark")
-  (get-game-id "John")
-  (get-todays-word)
-  (get-active-games nil))

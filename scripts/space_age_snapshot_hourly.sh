a53f9eee9052a7fa9fe8297477c434d66a17a50d
Eoin Carney <eoincarney0@gmail.com>
2024-05-31 21:37:40 +0100

    rough script for db snapshots

---
+++ scripts/space_age_snapshot_hourly.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+DB=""
+destination=""
+
+timestamp=$(date +"%Y%m%d%H%M%S")
+
+cp "$DB" "$destination$timestamp-games.db"
+
+count=$(find "$destination" -type f | wc -l)
+
+if [ "$count" -lt 9 ]
+then
+    exit 0
+else
+    rm -- "$destination/$(ls -rt $destination | head -n 1)"
+fi
